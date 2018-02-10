8f10313a860c17f9a204ed76e2bf339ae13a9293
Dmitry Goryunov <d.f.goryunov@gmail.com>
2018-02-10 12:51:12 +0100

    Initial implementation

---
+++ src/init.lua
@@ -0,0 +1,19 @@
+--init.lua
+print("Starting...")
+print("Version 1.0.2")
+wifi.setmode(wifi.STATION)
+wifi.sta.config {ssid="Gorun",pwd="sinisterkid"}
+--wifi.sta.config("Gorun_mob","sinisterkid")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function()
+    if wifi.sta.getip() == nil then
+        print("IP unavaiable, Waiting...")
+    else
+        tmr.stop(1)
+        print("Started")
+        print("ESP8266 mode is: " .. wifi.getmode())
+        print("The module MAC address is: " .. wifi.ap.getmac())
+        print("Config done, IP is "..wifi.sta.getip())
+        dofile("server.lua")
+    end
+end)
---
+++ src/server.lua
@@ -0,0 +1,77 @@
+local port = 80
+local enabled = false
+print("Starting server version 1.0.0 at port ", port)
+
+function statusController (request, next) 
+    if(request.path == "/status") then
+        if(request.method == "GET") then
+            if(enabled) then
+                return {status=200, body="{\"status\":\"enabled\"}"}
+            else
+                return {status=200, body="{\"status\":\"disabled\"}"}
+            end
+        elseif(request.method == "PUT") then
+            enabled = request.params.status == "enabled"
+            return {status=200}
+        end
+    else
+        return next(request)
+    end
+end
+
+function notFoundController (request)
+    return {status=404}
+end
+
+function formatResponse(result)
+    local response = ""
+        .. "HTTP/1.1 " .. result.status
+    if(result.body ~= nil) then
+        response = response 
+            .. "\r\nContent-Length: " .. string.len(result.body)
+            .. "\r\nContent-Type: json/application"
+            .. "\r\n"
+            .. "\r\n" .. result.body
+    end
+
+    return response
+end
+
+function parseRequest(request)
+    local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP")
+    if(method == nil)then
+        _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP")
+    end
+    local params = {}
+    if (vars ~= nil)then
+        for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+            params[k] = v
+        end
+    end
+
+    return {
+        path=path,
+        method=method,
+        params=params
+    }
+end
+
+srv=net.createServer(net.TCP)
+srv:listen(port,function(conn)
+    conn:on("receive", function(client, rawRequest)
+        local request = parseRequest(rawRequest)
+
+        local result = statusController(
+            request,
+            notFoundController)
+
+        response = formatResponse(result)
+        print(response)
+        
+        client:send(response, function(sent)
+            client:close()
+            collectgarbage()
+        end
+        )
+    end)
+end)
